c1da5d56d2e612c86fb4a38e4714ae73314baa42
Malinkin Evgenii <malinkin@mail.ru>
2023-02-16 15:59:33 +0300

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,20 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = Math.pow(b, 2)-(4*a*c);
+  if (d < 0) {
+    //return arr;
+  } else if (d == 0){
+    arr[0] = (-b/(2*a));
+  } else if (d > 0) {
+    arr[0] = (-b + Math.sqrt(d) )/(2*a);
+    arr[1] = (-b - Math.sqrt(d) )/(2*a); 
+  }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
+
+  return totalAmount;
 }
