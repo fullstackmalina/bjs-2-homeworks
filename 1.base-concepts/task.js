24841876c928053b6873fd3ecac83e02aec38609
”fullstackmalina” <malinkin@mail.ru>
2023-03-12 16:21:44 +0300

    First commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,20 +1,52 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  let d = Math.pow(b, 2)-(4*a*c);
-  if (d < 0) {
-    //return arr;
-  } else if (d == 0){
-    arr[0] = (-b/(2*a));
-  } else if (d > 0) {
-    arr[0] = (-b + Math.sqrt(d) )/(2*a);
-    arr[1] = (-b - Math.sqrt(d) )/(2*a); 
-  }
-  return arr;
+  let diskrim = b ** 2 -(4 * a * c);
+  let resZero;
+  // console.log("diskrim - " + diskrim);
+  if (diskrim < 0){
+    return arr;
+  } else if (diskrim == 0){
+
+    arr.push(-b/(2*a));
+    // console.log("diskrim=0 - " + (-b/(2*a)));
+    return arr;
+
+  } else if (diskrim > 0){
+    arr.push((-b + Math.sqrt(diskrim))/(2*a));
+    arr.push((-b - Math.sqrt(diskrim))/(2*a));
+    return arr;
+  } 
+  
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  let test;
+  let price;
+  let body;
+  let totalCost;
   
 
-  return totalAmount;
+  if (isFinite(percent)){
+    if (isFinite(contribution)){
+      if (isFinite(amount)){
+        if (isFinite(countMonths)){
+          percent = percent/100/12;
+            
+          body = amount - contribution;
+          test =  (percent + (percent / (((1 + percent)**countMonths) - 1)));
+          price =  body * test;
+            
+          totalCost =  price * countMonths;
+
+          totalCost = parseFloat(totalCost.toFixed(2));
+          return totalCost;
+
+        } else return false;//"countMonths - не число";
+      } else return false;//"amount - не число";
+    } else return false;//"contribution - не число";
+  } else return false;//"percent - не число";
+
 }
+
+console.log("Ответ - " + calculateTotalMortgage(10, 0, 50000, 12));
--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,24 @@
 function compareArrays(arr1, arr2) {
+    let result = arr1.every(function(element,index){
+    return (arr1.length === arr2.length && element===arr2[index]);
+    });
+    return result;
+    }
+    
+    function getUsersNamesInAgeRange(users, gender) {
+    if (Array.isArray(users) && users.length>0 && (users.findIndex(rez => rez.gender == gender)>-1)){
+    let filteredUsers = users.filter(rez => rez.gender == gender);
+    if (filteredUsers.length==0){
+    return 0;
+    } else {
+    let result = filteredUsers.reduce((accumulator, currentValue) => accumulator + currentValue.age, 0) / filteredUsers.length;
+    return result;
+    }
+    } else {
+    return 0;
+    }
+    }
+    
+    
   
-}
-
-function getUsersNamesInAgeRange(users, gender) {
-  
-}
+    
