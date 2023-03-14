0fd917a92e9c903a565ac1994249cc72a1307f7e
”fullstackmalina” <malinkin@mail.ru>
2023-03-14 17:04:23 +0300

    Second commit

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,70 @@
-//Задача № 1
+//                            task 1
+
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+
+    function wrapper(...args) {
+        const hash = args.join(',') // получаем правильный хэш
+        let idx = cache.findIndex((cacheRecord) => cacheRecord.hash === hash); // ищем элемент, хэш которого равен нашему хэшу
+        if (idx !== -1) { // если элемент не найден
+            console.log("Из кэша: " + cache[idx].result); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+            return "Из кэша: " + cache[idx].result;
+        }
+
+        let result = func.call(this, ...args); // в кэше результата нет - придётся считать
+        cache.push({ hash, result }); // добавляем элемент с правильной структурой
+        if (cache.length > 5) {
+            cache.push({ hash, result }); // если слишком много элементов в кэше надо удалить самый старый (первый) 
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;
+    }
+    return wrapper;
+
 }
 
-//Задача № 2
-function debounceDecoratorNew(func, delay) {
-  
+//                            task 2
+
+function debounceDecoratorNew(func, ms) {
+
+    let timeout = null;;
+    let repeatCall = false;
+
+    function wrapper(...rest) {
+
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            repeatCall = false;
+            return func.call(...rest);
+        }, delay);
+        if (!repeatCall) {
+            repeatCall = true;
+            func.call(...rest);
+        }
+    }
+
+    return wrapper;
+}
+
+
+//                             task 3     
+function debounceDecorator2(func, ms) {
+
+    let timer = null;
+    let timerStatus = false;
+    wrapper.count = 0;
+
+    function wrapper(...rest) {
+        ++wrapper.count;
+        if (!timerStatus) {
+            func.call(this, ...rest);
+        };
+        timerStatus = true;
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            func.call(this, ...rest);
+            timerStatus = false;
+        }, ms);
+    };
+    return wrapper;
 }
