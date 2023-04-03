9e302592877b0a9a1dcccf500d07f15c562471a9
”fullstackmalina” <malinkin@mail.ru>
2023-04-03 08:45:55 +0300

    Исправленный commit

--- 7.async/task.js
+++ 7.async/task.js
@@ -3,92 +3,66 @@ class AlarmClock {
         this.alarmCollection = [];
         this.timerId = null;
     }
-    addClock(time, callback, id) {
-        if (id === undefined) {
-            throw new Error('error text');
+
+    addClock(id, time, callback) {
+        if (!id) {
+            throw new Error('id не передан');
         }
-        if (this.alarmCollection.some(alarm => alarm.id === id)) {
-            console.error("Звонок с таким id уже существует!");
+
+        let validId = this.alarmCollection.some((item) => id == this.alarmCollection[0].id);
+        if (validId == true) {
+            console.error('Звонок с данным id уже существует');
             return;
         }
-        this.alarmCollection.push({ time, callback, id });
+        let bell = { id, time, callback };
+        this.alarmCollection.push(bell);
     }
-    removeClock(id) {
-        if (this.alarmCollection.some((alarm) => alarm.id === id)) {
-            this.alarmCollection = this.alarmCollection.filter((alarm) => alarm.id !== id);
-            return true;
+
+    removeClock(delId) {
+        let beforeAlarmColl = this.alarmCollection.length;
+        let validDel = this.alarmCollection.findIndex((item, idx) => delId == this.alarmCollection[0].id);
+        if (validDel !== -1) {
+            this.alarmCollection.splice(validDel, 1);
         }
-        return false;
+        let afterAlarmColl = this.alarmCollection.length;
+        return beforeAlarmColl !== afterAlarmColl;
     }
+
     getCurrentFormattedTime() {
-        let currentTime = ("0" + new Date().getHours()).slice(-2) + ":"
-            + ("0" + new Date().getMinutes()).slice(-2);
-        return currentTime;
+        let currentDate = new Date().toLocaleTimeString("ru", { hour: "2-digit", minute: "2-digit" });
+        return currentDate;
     }
+
     start() {
-        let checkClock = (alarm) => {
-            let time = this.getCurrentFormattedTime();
-            console.log(time);
-            if (alarm.time === time) {
-                alarm.callback();
-                return;
+        if (this.timerId != null) {
+            return;
+        }
+        let interval = setInterval(() => {
+            this.alarmCollection.forEach((el) => checkClock(el));
+        }, 3000);
+        this.timerId = interval;
+
+        const checkClock = (el) => {
+            if (this.getCurrentFormattedTime() === el.time) {
+                return el.callback();
             }
-        }
-        if (this.timerId === null) {
-            this.timerId = setInterval(() => this.alarmCollection.forEach(alarm => checkClock(alarm)), 1000);
-            console.log(this.timerId);
-        }
-        return;
+        };
     }
+
     stop() {
-        if (this.timerId !== null) {
-            this.timerId = null;
-            clearInterval(this.timerId);
+        if (this.timerId == null) {
+            return;
         }
+        clearInterval(this.timerId);
+        this.timerId == null;
     }
+
     printAlarms() {
-        console.log(`Печать всех будильников в количестве: ${this.alarmCollection.length}`);
-        this.alarmCollection.forEach(alarm => {
-            console.log(`Будильник № ${alarm.id} заведен на ${alarm.time}`);
-        });
+        this.alarmCollection.forEach((item) => console.log(`id: ${item.id}, time: ${item.time}`));
     }
+
     clearAlarms() {
         this.stop();
         this.alarmCollection = [];
     }
 }
-
-// Пример использования *AlarmClock*
-
-function testCase() {
-    let alarmClock = new AlarmClock;
-    let currentTime = alarmClock.getCurrentFormattedTime();
-    let currentTimePlusOneMinute = currentTime.split(":")[0] +
-        ":" + ("0" + (Number.parseInt(currentTime.split(":")[1]) + 1)).slice(-2);  /* Не стал уж проверять, больше ли количество минут, чем 60 */
-    let currentTimePlusTwoMinute = currentTime.split(":")[0] +
-        ":" + ("0" + (Number.parseInt(currentTime.split(":")[1]) + 2)).slice(-2);
-
-    alarmClock.addClock(currentTime, () => {
-        console.log("Подъем!");
-        console.log("Подъем!!");
-        console.log("Подъем!!!");
-    }, 1);
-
-    alarmClock.addClock(currentTimePlusOneMinute, () => {
-        console.log("Серьезно, пора вставать!");
-        alarmClock.removeClock(2)
-    }, 2);
-
-    alarmClock.addClock(currentTimePlusTwoMinute, () => {
-        console.log("ВСТАВАААААЙ!!!");
-        alarmClock.stop();
-        alarmClock.clearAlarms();
-        alarmClock.printAlarms();
-    }, 3);
-
-    alarmClock.start();
-    alarmClock.stop();
-    alarmClock.printAlarms();
-
-}
-testCase();
